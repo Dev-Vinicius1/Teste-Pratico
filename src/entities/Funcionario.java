ba50b04b551b17ca336f2edbcd23226a3e4d1657
vinicius_augusto <hat.ubuntu@gmail.com>
2024-06-19 10:55:01 -0300

    criação dos métodos adicionar e remover funcionario

--- src/entities/Funcionario.java
+++ src/entities/Funcionario.java
@@ -1,5 +1,31 @@
 package entities;
 
-public class Funcionario{
+import java.math.BigDecimal;
+import java.time.LocalDate;
 
+public class Funcionario extends Pessoa{
+    private BigDecimal salario;
+    private String funcao;
+
+    public Funcionario(String nome, LocalDate dataDeNascimento, BigDecimal salario, String funcao) {
+        super(nome, dataDeNascimento);
+        this.salario = salario;
+        this.funcao = funcao;
+    }
+
+    public BigDecimal getSalario() {
+        return salario;
+    }
+
+    public void setSalario(BigDecimal salario) {
+        this.salario = salario;
+    }
+
+    public String getFuncao() {
+        return funcao;
+    }
+
+    public void setFuncao(String funcao) {
+        this.funcao = funcao;
+    }
 }
--- src/services/FuncionarioServices.java
+++ src/services/FuncionarioServices.java
@@ -1,5 +1,18 @@
 package services;
 
+import entities.Funcionario;
+
+import java.util.List;
+
 public class FuncionarioServices {
 
+    private List<Funcionario> funcionarios;
+
+    public void adicionarNovoFuncionario(Funcionario funcionario){
+        funcionarios.add(funcionario);
+    }
+
+    public void removerFuncionario(String nome){
+        funcionarios.removeIf(funcionario -> funcionario.getNome().equals(nome));
+    }
 }
